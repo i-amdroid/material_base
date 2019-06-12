a58cd40dd2c3b2f5342285446acd43fda9fae7b3
Andrey Yurtaev <mdrdnt@gmail.com>
2019-06-12 22:50:30 +0000

    by iamdroid: Added breakpoints. Updated Gulp.

--- starterkits/material_compile/gulpfile.js
+++ starterkits/material_compile/gulpfile.js
@@ -1,6 +1,4 @@
 var gulp = require('gulp');
-var watch = require('gulp-watch');
-var batch = require('gulp-batch');
 var sass = require('gulp-sass');
 var sasslint = require('gulp-sass-lint');
 var eslint = require('gulp-eslint');
@@ -17,62 +15,56 @@ var sassOptions = {
   includePaths: ['./node_modules/breakpoint-sass/stylesheets']
 };
 
-var autoprefixerOptions = {
-  browsers: ['> 1%']
-};
-
 // tasks
 
-gulp.task('sass', function () {
-  return gulp.src(SASS + '/**/*.scss')
+gulp.task('sass', done => {
+  gulp.src(SASS + '/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass.sync(sassOptions).on('error', sass.logError))
-    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(autoprefixer({}))
     .pipe(sourcemaps.write('../css'))
     .pipe(gulp.dest(CSS));
+  done();
 });
 
-gulp.task('sass-lint', function () {
-  return gulp.src(SASS + '/**/*.scss')
+gulp.task('sass-lint', done => {
+  gulp.src(SASS + '/**/*.scss')
     .pipe(sasslint())
     .pipe(sasslint.format())
-    .pipe(sasslint.failOnError());
+    .pipe(sasslint.failOnError())
+  done();
 });
 
-gulp.task('js-lint', function () {
-  return gulp.src(JS + '/**/*.js')
+gulp.task('js-lint', done => {
+  gulp.src(JS + '/**/*.js')
     .pipe(eslint())
     .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
+    .pipe(eslint.failAfterError())
+  done();
 });
 
-gulp.task('imagemin', function() {
+gulp.task('imagemin', done => {
   gulp.src(IMG + '/src/*')
     .pipe(imagemin())
     .pipe(gulp.dest(IMG));
+  done();
 });
 
 // build without lint
 
-gulp.task('build', ['sass', 'imagemin']);
+gulp.task('build', gulp.series('sass', 'imagemin'));
 
 gulp.task('watch', function () {
-  watch(SASS + '/**/*.scss', batch(function (events, done) {
-    gulp.start('build', done);
-  }));
+  return gulp.watch(SASS + '/**/*.scss', gulp.series(gulp.series('sass', 'imagemin')));
 });
 
 // build with lint
 
-gulp.task('build-lint', ['sass-lint', 'sass', 'imagemin', 'js-lint']);
+gulp.task('build-lint', gulp.series('sass-lint', 'sass', 'imagemin', 'js-lint'));
 
 gulp.task('watch-lint', function () {
-  watch(SASS + '/**/*.scss', batch(function (events, done) {
-    gulp.start('build-int', done);
-  }));
+  return gulp.watch(SASS + '/**/*.scss', gulp.series(gulp.series('sass-lint', 'sass', 'imagemin', 'js-lint')));
 });
 
-// default task
-
-gulp.task('default', ['build', 'watch']);
-//gulp.task('default', ['build-lint', 'watch-lint']);
+gulp.task('default', gulp.series('build', 'watch'));
+// gulp.task('default', gulp.series('build-lint', 'watch-lint'));
